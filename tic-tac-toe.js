ad6c63ee8534fbf2a60e71151fdb2e5272fcad35
gdevl <throwawayemailaddy@protonmail.com>
2020-08-06 19:30:19 -0400

    almost working. clickcount sill incrementing more than necessary.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -11,28 +11,77 @@ function even(n) {
     return n % 2 === 0;
 }
 
-const isSquareOccupied = (array, el) => {
-    return array.includes(el);
+const isSquareOccupied = (array1, array2, el) => {
+    return (array1.includes(el) || array2.includes(el));
 }
-
+// console.log(isSquareOccupied(['square-0', 'square-1'], 'square-0'));
 const gridClicker = () => {
-    let gridSquares = document.querySelectorAll('.square'); // holds the squares divs
-    gridSquares.forEach(gridSquare => {
 
+    let gridSquares = document.querySelectorAll('.square'); // holds the squares divs
+    
+
+    // gridSquares.forEach(gridSquare => {
+
+    //     gridSquare.addEventListener('click', event => {
+    //         // every time we click, we add the
+            // let current = event.target;
+            // let currentId = event.target.id;
+
+    //         if (isSquareOccupied(playerX, playerO, currentId)) {
+    //             return;
+    //         }
+            
+
+    //         console.log('current= ' + current);
+    //         console.log('currentId= ' + currentId);
+
+            // if (!even(clickCount)) {
+            //     current.innerHTML = "<img src='./images/player-x.svg'/>";
+            //     playerX.push(currentId);
+            //     clickCount++;
+            //     console.log('playerX: ' + playerX);
+            //     console.log(clickCount);
+            //     return;
+            // } else if (even(clickCount)) {
+            //     current.innerHTML = "<img src='./images/player-o.svg'/>";
+            //     playerO.push(currentId);
+            //     clickCount++;
+            //     console.log('playerO: ' + playerO);
+            //     console.log(clickCount);
+            //     return;
+            // }
+    //     });
+    // });
+
+    for (let i = 0; i < gridSquares.length; i ++) {
+        let gridSquare = gridSquares[i];
         gridSquare.addEventListener('click', event => {
-            // every time we click, we add the
-            let current = event.target
-            console.log(current)
+            let current = event.target;
+            let currentId = event.target.id;
+
+            if (isSquareOccupied(playerX, playerO, currentId)) {
+                return;
+            }
+
             if (!even(clickCount)) {
                 current.innerHTML = "<img src='./images/player-x.svg'/>";
-                // add square id to the player x array.
-                // playerX.push(current);
-            } else {
+                playerX.push(currentId);
+                clickCount+=1;
+                console.log('playerX: ' + playerX);
+                console.log(clickCount);
+                return;
+            } else if (even(clickCount)) {
                 current.innerHTML = "<img src='./images/player-o.svg'/>";
+                playerO.push(currentId);
+                clickCount+=1;
+                console.log('playerO: ' + playerO);
+                console.log(clickCount);
+                return;
             }
-            clickCount++
-        });
-    });
+
+        })
+    }
+
 };
 
 gridClicker();
