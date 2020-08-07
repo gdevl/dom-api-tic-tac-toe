95632d3ee63fc0b3109f013aceac2d476afdc1b7
gdevl <throwawayemailaddy@protonmail.com>
2020-08-07 13:43:58 -0400

    requirement 2 complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -11,47 +11,17 @@ function even(n) {
     return n % 2 === 0;
 }
 
-const isSquareOccupied = (array1, array2, el) => {
-    return (array1.includes(el) || array2.includes(el));
+// const isSquareOccupied = (array1, array2, el) => {
+//     return (array1.includes(el) || array2.includes(el)); // 
+// }
+
+const isSquareOccupied = el => {
+    return el === '';
 }
 // console.log(isSquareOccupied(['square-0', 'square-1'], 'square-0'));
 const gridClicker = () => {
 
     let gridSquares = document.querySelectorAll('.square'); // holds the squares divs
-    
-
-    // gridSquares.forEach(gridSquare => {
-
-    //     gridSquare.addEventListener('click', event => {
-    //         // every time we click, we add the
-            // let current = event.target;
-            // let currentId = event.target.id;
-
-    //         if (isSquareOccupied(playerX, playerO, currentId)) {
-    //             return;
-    //         }
-            
-
-    //         console.log('current= ' + current);
-    //         console.log('currentId= ' + currentId);
-
-            // if (!even(clickCount)) {
-            //     current.innerHTML = "<img src='./images/player-x.svg'/>";
-            //     playerX.push(currentId);
-            //     clickCount++;
-            //     console.log('playerX: ' + playerX);
-            //     console.log(clickCount);
-            //     return;
-            // } else if (even(clickCount)) {
-            //     current.innerHTML = "<img src='./images/player-o.svg'/>";
-            //     playerO.push(currentId);
-            //     clickCount++;
-            //     console.log('playerO: ' + playerO);
-            //     console.log(clickCount);
-            //     return;
-            // }
-    //     });
-    // });
 
     for (let i = 0; i < gridSquares.length; i ++) {
         let gridSquare = gridSquares[i];
@@ -59,25 +29,62 @@ const gridClicker = () => {
             let current = event.target;
             let currentId = event.target.id;
 
-            if (isSquareOccupied(playerX, playerO, currentId)) {
-                return;
+            // if (isSquareOccupied(playerX, playerO, currentId)) {
+            //     console.log(`Square is occupied!`);
+            //     return;
+            // }
+            
+            console.log(`currentId= ${currentId}`);
+            console.log(`square occupied? ${isSquareOccupied(currentId)}`);
+            console.log(`previous clickCount= ${clickCount}`);
+            // console.log(`Is clickCount even? ${even(clickCount)}`);
+            console.log(`----------`);
+            
+            
+
+            if (even(clickCount) === false) {
+                if (isSquareOccupied(currentId)) {
+                    return;
+                } else {
+                    current.innerHTML = "<img src='./images/player-x.svg'/>";
+                    playerX.push(currentId);
+                    clickCount += 1;
+                    console.log(`incremented clickCount= ${clickCount}`);
+                    console.log('playerX: ' + playerX);
+                    console.log(`----------`);
+                    return;
+                } 
+            }
+            
+            if (even(clickCount) === true) {
+                if (isSquareOccupied(currentId)) {
+                    return;
+                } else {
+                    current.innerHTML = "<img src='./images/player-o.svg'/>";
+                    playerO.push(currentId);
+                    clickCount += 1;
+                    console.log(`incrememented clickCount= ${clickCount}`);
+                    console.log('playerO: ' + playerO);
+                    console.log(`----------`);
+                    return;
+                }
             }
 
-            if (!even(clickCount)) {
-                current.innerHTML = "<img src='./images/player-x.svg'/>";
-                playerX.push(currentId);
-                clickCount+=1;
-                console.log('playerX: ' + playerX);
-                console.log(clickCount);
-                return;
-            } else if (even(clickCount)) {
-                current.innerHTML = "<img src='./images/player-o.svg'/>";
-                playerO.push(currentId);
-                clickCount+=1;
-                console.log('playerO: ' + playerO);
-                console.log(clickCount);
-                return;
-            }
+            // if (!even(clickCount)) {
+            //     current.innerHTML = "<img src='./images/player-x.svg'/>";
+            //     playerX.push(currentId);
+            //     clickCount+=1;
+            //     console.log('playerX: ' + playerX);
+            //     console.log(clickCount);
+            //     return;
+            // } else if (even(clickCount)) {
+            //     current.innerHTML = "<img src='./images/player-o.svg'/>";
+            //     playerO.push(currentId);
+            //     clickCount+=1;
+            //     console.log('playerO: ' + playerO);
+            //     console.log(clickCount);
+            //     return;
+            // }
 
         })
     }
